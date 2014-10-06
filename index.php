46dd8892cfb86cf28f3f07641e11d660018ec592
Maria Emmanouil <emmanouil.maria@gmail.com>
2014-10-06 23:28:37 +0300

    style and index added

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>CMS</title>
+		<link rel="stylesheet" type="text/css" href="assets/style.css">
+	</head>
+	<body>
+		<div class="container">
+			<a href="index.php" id="logo">CMS</a>
+
+			<ol>
+				<li><a href="article.php?id=1">Article Title</a>-<small>posted 10th Jan</small></li>
+			</ol>
+			
+		</div>
+	</body>
+</html>
